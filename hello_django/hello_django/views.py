926c681e180075dd8555092414a3b27e0dbf66f9
kot3u <kot3uko_1337@mail.ru>
2022-10-25 10:33:17 +0300

    start_files

---
+++ hello_django/hello_django/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def about(request):
+    a = 5+6
+    return render(request, 'about.html', {'gretting':a})
+def home(request):
+    return HttpResponse('This is my home')
