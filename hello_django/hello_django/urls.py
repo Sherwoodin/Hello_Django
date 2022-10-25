81933d99561caeec2f748cd18e20c916d7c620f6
kot3u <kot3uko_1337@mail.ru>
2022-10-25 11:39:38 +0300

    start_files

--- hello_django/hello_django/urls.py
+++ hello_django/hello_django/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.about),
-    path('home/', views.home)
+    path('home/', views.home),
+    path('reverse/', views.reverse)
 ]
--- hello_django/hello_django/views.py
+++ hello_django/hello_django/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def about(request):
-    a = 5+6
-    return render(request, 'about.html', {'gretting':a})
-def home(request):
-    return HttpResponse('This is my home')
+        a = 5 + 6
+        return render(request, 'about.html', {'gretting': a})
+
+def reverse (request):
+        user_text = request.GET['username']
+        reverse = user_text[::-1]
+        return render(request, 'reverse.html', {'word': reverse})
+def home (request):
+        return HttpResponse('This is my home')
